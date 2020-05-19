613e19ff112c31821e8ee3a810c45cb5df1e5d1e
ceyeoh <niezyeoh1999@gmail.com>
2020-05-20 04:44:09 +0800

    add reorder and wrap function

--- documentScanner.py
+++ documentScanner.py
@@ -6,6 +6,8 @@ heightImg = 640
 kernel = np.ones((5, 5))
 
 cam = cv2.VideoCapture(0)
+
+
 # cam.set(3, widthImg)
 # cam.set(4, heightImg)
 
@@ -36,6 +38,34 @@ def getContours(img):
 
     cv2.drawContours(imgContour, biggest, -1, (0, 0, 255), 20)
     return biggest
+
+
+def reorder(points):
+    points = points.reshape((4, 2))
+    newPoints = np.zeros((4, 1, 2), np.int32)
+    add = points.sum(1)
+    # print('add',add)
+    newPoints[0] = points[np.argmin(add)]
+    newPoints[3] = points[np.argmax(add)]
+    diff = np.diff(points, axis=1)
+    newPoints[1] = points[np.argmin(diff)]
+    newPoints[2] = points[np.argmax(diff)]
+    # print('newpoints',newpoints)
+
+    return newPoints
+
+
+def wrap(img, biggest):
+    biggest = reorder(biggest)
+    p1 = np.float32(biggest)
+    p2 = np.float32([0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg])
+    matrix = cv2.getPerspectiveTransform(p1, p2)
+    imgOutput = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+    imgCropped = imgOutput[20:imgOutput.shape[0] - 20, 20:imgOutput.shape[1] - 20]
+    imgCropped = cv2.resize(imgCropped, (widthImg, heightImg))
+
+    return imgCropped
 
 
 while True:
